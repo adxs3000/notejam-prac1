4d5c38bd2c3a9b350bba3bcf20d79039978b05d9
adxs3000 <archit.dadhich@xenonstack.com>
2024-09-03 18:00:13 +0530

    Update __init__.py

--- flask/notejam/__init__.py
+++ flask/notejam/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
 from notejam.config import (
